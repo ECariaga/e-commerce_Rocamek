4054260071d4790c5dab8c63fca23ec2cf7eed8d
Evitha-CS <ecariaga.sandoval@gmail.com>
2025-03-19 17:23:02 -0300

    Creacion de componente navbar y header
    Se crearon las rutas necesarias para el navbar con react-router-dom

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,73 @@
+import styles from "./Header.module.css";
+import Navbar from "../navbar/Navbar";
+import { IoSearchOutline } from "react-icons/io5";
+import { IoCartOutline } from "react-icons/io5";
+import { HiOutlineUser } from "react-icons/hi2";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
+
+function Header() {
+  const [isSearchExpanded, setIsSearchExpanded] = useState(false);
+  const toggleSearch = () => {
+    setIsSearchExpanded(!isSearchExpanded);
+  };
+  return (
+    <section className={styles.header}>
+      <section className={styles.header_top}>
+        <section className={styles.header_top_logo}>
+          <a href="/" className={styles.header_logo}>
+            ROCAMEK
+          </a>
+        </section>
+        <section className={styles.header_top_search}>
+          <section className={styles.header_top_navigation}>
+            <div
+              className={`${styles.search_container} ${
+                isSearchExpanded ? styles.expanded : ""
+              }`}
+            >
+              {isSearchExpanded && (
+                <input
+                  type="text"
+                  className={styles.search_input}
+                  placeholder="Buscar"
+                />
+              )}
+              <button className={styles.search_button} onClick={toggleSearch}>
+                <IoSearchOutline />
+                {/* icono de lupa */}
+              </button>
+              {isSearchExpanded && (
+                <button className={styles.close_button} onClick={toggleSearch}>
+                  <IoClose />
+                </button>
+              )}
+            </div>
+            {!isSearchExpanded && (
+              <div className={styles.link_container}>
+                <a href="/" className={styles.link}>
+                  <IoCartOutline className={styles.icon} />{" "}
+                  {/* icono de carro */}
+                </a>
+                <a href="/" className={styles.link}>
+                  <HiOutlineUser className={styles.icon} />
+                  {/* icono de usuario */}
+                </a>
+                <div className={styles.header_top_navbar}>
+                  <Navbar />
+                </div>
+              </div>
+            )}
+          </section>
+        </section>
+      </section>
+      <section className={styles.header_botton}>
+        <section className={styles.header_botton_navbar}>
+          <Navbar />
+        </section>
+      </section>
+    </section>
+  );
+}
+
+export default Header;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import styles from "./Navbar.module.css";
+import { IoMenuSharp } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
+import { Outlet, Link } from "react-router-dom";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <section className={styles.container}>
+      {!isOpen && (
+        <span className={styles.menu_icon} onClick={toggleMenu}>
+          <IoMenuSharp />
+          {/*iconos de menu*/}
+        </span>
+      )}
+      <div className={`${styles.navbar} ${isOpen ? styles.active : ""}`}>
+        <Link to="/">Inicio</Link>
+        <Link to="/products">Productos</Link>
+        <Link to="/about">Nosotros</Link>
+        <Link to="/contact">Contáctenos</Link>
+        {isOpen && (
+          <span className={styles.close_icon} onClick={toggleMenu}>
+            <IoClose />
+          </span>
+        )}
+        <Outlet />
+      </div>
+    </section>
+  );
+}
+
+export default Navbar;
